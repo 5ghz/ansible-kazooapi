042b6c0411b72d07e23960982e1bc53f698dfcd8
Vladimir <pershin87@yandex.ru>
2018-01-22 18:09:12 +0700

    Update kazoo_accounts.py
    Final version

--- library/kazoo_accounts.py
+++ library/kazoo_accounts.py
@@ -2,12 +2,11 @@
 
 from ansible.module_utils.basic import *
 import kazoo
-import inspect
-#import ansible_util
 
-class kazoo_helper(object):
+
+class KazooHelper(object):
     @classmethod
-    def auth(self, auth_module=None):
+    def auth(cls, auth_module=None):
         auth_user = auth_module.params.get('auth_user', None)
         auth_pass = auth_module.params.get('auth_pass', None)
         auth_realm = auth_module.params.get('auth_realm', None)
@@ -24,7 +23,7 @@ class kazoo_helper(object):
         return client
 
     @classmethod
-    def run_handler(self, action_func=None,  ansible_module = None):
+    def run_handler(cls, action_func=None, ansible_module=None):
         msgs = []
 
         try:
@@ -35,73 +34,81 @@ class kazoo_helper(object):
             else:
                 ansible_module.fail_json(msg='ERROR: Exception caught: %s' % e)
 
-class account_operation(kazoo_helper):
+
+class AccountOperation(KazooHelper):
     def __init__(self, ansible_module=None):
-        self.client = kazoo_helper.auth(auth_module=ansible_module)
+        self.client = KazooHelper.auth(auth_module=ansible_module)
         self.ansible_module = ansible_module
         self.account_data = {}
 
-        if (ansible_module.params['state'] is None):
+        if ansible_module.params['state'] is None:
             module.fail_json(msg='State is missing')
 
-        if (ansible_module.params['account_name'] is not None):
+        if ansible_module.params['account_name'] is not None:
             self.account_data['name'] = ansible_module.params['account_name']
 
-        if (ansible_module.params['state'] is not None):
+        if ansible_module.params['state'] is not None:
             self.account_data['state'] = ansible_module.params['state']
 
-        if (ansible_module.params['timezone'] is not None):
+        if ansible_module.params['timezone'] is not None:
             self.account_data['timezone'] = ansible_module.params['timezone']
 
-        if (ansible_module.params['call_restriction'] is not None):
+        if ansible_module.params['call_restriction'] is not None:
             self.account_data['call_restriction'] = ansible_module.params['call_restriction']
 
-        if (ansible_module.params['caller_id'] is not None):
+        if ansible_module.params['caller_id'] is not None:
             self.account_data['caller_id'] = ansible_module.params['caller_id']
 
-        if (ansible_module.params['dial_plan'] is not None):
+        if ansible_module.params['dial_plan'] is not None:
             self.account_data['dial_plan'] = ansible_module.params['dial_plan']
 
-        if (ansible_module.params['music_on_hold'] is not None):
+        if ansible_module.params['music_on_hold'] is not None:
             self.account_data['music_on_hold'] = ansible_module.params['music_on_hold']
 
-        if (ansible_module.params['preflow'] is not None):
+        if ansible_module.params['preflow'] is not None:
             self.account_data['preflow'] = ansible_module.params['preflow']
 
-        if (ansible_module.params['ringtones'] is not None):
+        if ansible_module.params['ringtones'] is not None:
             self.account_data['ringtones'] = ansible_module.params['ringtones']
 
-        if (ansible_module.params['notifications'] is not None):
+        if ansible_module.params['notifications'] is not None:
             self.account_data['notifications'] = ansible_module.params['notifications']
 
-        if (ansible_module.params['is_reseller'] is not None):
+        if ansible_module.params['is_reseller'] is not None:
             self.account_data['is_reseller'] = ansible_module.params['is_reseller']
 
-        if (ansible_module.params['wnm_allow_additions'] is not None):
+        if ansible_module.params['wnm_allow_additions'] is not None:
             self.account_data['wnm_allow_additions'] = ansible_module.params['wnm_allow_additions']
 
-        if (ansible_module.params['enabled'] is not None):
+        if ansible_module.params['enabled'] is not None:
             self.account_data['enabled'] = ansible_module.params['enabled']
 
-        if (ansible_module.params['billing_mode'] is not None):
+        if ansible_module.params['billing_mode'] is not None:
             self.account_data['billing_mode'] = ansible_module.params['billing_mode']
 
-        if (ansible_module.params['account_id'] is not None):
-            self.account_data['account_id'] = ansible_module.params['account_id']
+        if ansible_module.params['account_id'] is not None:
+            self.account_id = ansible_module.params['account_id']
 
     def create_account(self):
-        print("kazoo create account")
+        data = self.client.create_account(self.account_data)
+        return data, True
 
     def update_account(self):
-        print("kazoo update account")
+        if (hasattr(self, 'account_id') and not self.account_id) or (not hasattr(self, 'account_id')):
+            self.ansible_module.fail_json(msg="No account_id")
+        data = self.client.get_account(self.account_id)
+        data['data'].update(self.account_data)
+        data = self.client.update_account(self.account_id, data['data'])
+        return data, True
 
     def delete_account(self):
-        print("kazoo delete account")
+        if (hasattr(self, 'account_id') and not self.account_id) or (not hasattr(self, 'account_id')):
+            self.ansible_module.fail_json(msg="No account_id")
+        data = self.client.delete_account(self.account_id)
+        return data, True
 
 
 def main():
-    #account_data = {}
-    #item_id = None
 
     fields = {
         "auth_user": {"required": False, "type": "str"},
@@ -110,7 +117,7 @@ def main():
         "base_url": {"required": True, "type": "str"},
         "api_key": {"required": False, "type": "str"},
         "account_id": {"required": False, "type": "str"},
-        "state": { "choices" : ['enabled', 'disabled'], "required": True},
+        "state": {"choices": ['enabled', 'disabled', "update"], "required": True},
         "account_name": {"required": False, "type": "str"},
         "account_realm": {"required": False, "type": "str"},
         "timezone": {"required": False, "type": "str"},
@@ -129,22 +136,22 @@ def main():
     }
 
     ansible_module = AnsibleModule(argument_spec=fields, supports_check_mode=True)
-    account = account_operation(ansible_module)
+    account = AccountOperation(ansible_module)
 
+    if account.account_data['state'] == 'enabled':
+        (data, chand) = account.run_handler(action_func=account.create_account, ansible_module=ansible_module)
+        if chand:
+            ansible_module.exit_json(changed=chand, meta=data['data'])
 
-    if (account.account_data['state'] == 'enabled' and hasattr(account.account_data,'account_id') and \
-            not account.account_data['account_id']) or (account.account_data['state'] == 'enabled'
-                                                               and not hasattr(account.account_data,'account_id')):
-        res = account.run_handler(action_func=account.create_account, ansible_module = ansible_module)
     elif account.account_data['state'] == 'disabled':
-        res = account.run_handler(account.delete_account)
-    elif account.account_data['state'] == 'enabled' and hasattr(account.account_data,'account_id') and \
-            account.account_data['account_id']:
-        res = account.run_handler(account.update_account)
+        (data, chand) = account.run_handler(account.delete_account, ansible_module=ansible_module)
+        if chand:
+            ansible_module.exit_json(changed=chand, meta='delete account')
 
-    #response = {"hello": "world"}
-    response={"hello": res}
-    ansible_module.exit_json(changed=False, meta=response)
+    elif account.account_data['state'] == 'update':
+        (data, chand) = account.run_handler(account.update_account, ansible_module=ansible_module)
+        if chand:
+            ansible_module.exit_json(changed=chand, meta=data['data'])
 
 
 if __name__ == '__main__':
